de627ada9c582c16ac0e96906ba20520bbd893e8
mathinajohn <johnmainagk6@gmail.com>
2024-11-25 14:30:02 +0300

    First Commit

---
+++ main.js
@@ -0,0 +1,89 @@
+
+
+
+
+let link = document.getElementById("link");
+link.setAttribute("href","https://github.com/mathinajohn")
+link.textContent ="More Info";
+link.style.color = "white";
+link.style.fontFamily = "fontawesome";
+link.style.fontSize = "20px";
+
+
+let link = document.getElementById("linkedin");
+link.setAttribute("href","https://www.linkedin.com/in/john-mathina-220190332/")
+link.textContent ="";
+link.style.color = "";
+link.style.fontFamily = "fontawesome";
+link.style.fontSize = "20px";
+
+
+let link = document.getElementById("github");
+link.setAttribute("href","https://github.com/mathinajohn")
+link.textContent ="";
+link.style.color = "";
+link.style.fontFamily = "fontawesome";
+link.style.fontSize = "20px";
+
+
+let link = document.getElementById("instagram");
+link.setAttribute("href","https://www.instagram.com/johnmainagk6/?next=%2F")
+link.textContent ="";
+link.style.color = "";
+link.style.fontFamily = "fontawesome";
+link.style.fontSize = "20px";
+
+
+let link = document.getElementById("twitter");
+link.setAttribute("href","https://x.com/john_mathina")
+link.textContent ="";
+link.style.color = "";
+link.style.fontFamily = "fontawesome";
+link.style.fontSize = "20px";
+
+
+let link = document.getElementById("project-link1");
+link.setAttribute("href","https://github.com/mathinajohn/Cara-site")
+link.textContent ="Review Project";
+link.style.color = "white";
+link.style.fontFamily = "fontawesome";
+link.style.fontSize = "20px";
+
+
+let link = document.getElementById("project-link2");
+link.setAttribute("href","https://github.com/mathinajohn/Group-Kitchen")
+link.textContent ="Review Project";
+link.style.color = "white";
+link.style.fontFamily = "fontawesome";
+link.style.fontSize = "20px";
+
+
+let link = document.getElementById("project-link3");
+link.setAttribute("href","https://github.com/mathinajohn")
+link.textContent ="Review Project";
+link.style.color = "white";
+link.style.fontFamily = "fontawesome";
+link.style.fontSize = "20px";
+
+
+let link = document.getElementById("project-link4");
+link.setAttribute("href","https://github.com/mathinajohn/Grocery-app")
+link.textContent ="Review Project";
+link.style.color = "white";
+link.style.fontFamily = "fontawesome";
+link.style.fontSize = "20px";
+
+
+let link = document.getElementById("project-link5");
+link.setAttribute("href","https://github.com/mathinajohn/project-one")
+link.textContent ="Review Project";
+link.style.color = "white";
+link.style.fontFamily = "fontawesome";
+link.style.fontSize = "20px";
+
+let link = document.getElementById("project-link6");
+link.setAttribute("href","https://github.com/mathinajohn/Project-6")
+link.textContent ="Review Project";
+link.style.color = "white";
+link.style.fontFamily = "fontawesome";
+link.style.fontSize = "20px";
